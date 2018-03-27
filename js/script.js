6903a4113fb1a2db5ea6577f61f1f357355a9fe5
Unknown <javascript5@hotmail.com>
2018-03-27 21:47:51 +0700

    [Update]feed.html เสร็จแล้ว ที่เหลือกินไก่

--- js/script.js
+++ js/script.js
@@ -14,22 +14,21 @@ $(document).ready(function() {
 
 
   $('.new_post').click(function() {
-var filename = $('#image-upload').val();
-console.log(filename);
-    $(".overlay").fadeIn(500);
+    var filename = $('#image-upload').val();
+    $(".overlay").fadeIn(200);
     $(".new_post_field").show();
     $(".new_post_button").show();
     $('.bottom_new_post > .separator').show();
 
     if (filename == "") {
       $("#image-preview").hide()
-    }else{
+    } else {
       $("#image-preview").show();
     }
   });
 
   $(".overlay").not(".text").click(function() {
-    $(".overlay").fadeOut(500);
+    $(".overlay").fadeOut(200);
     $(".new_post_field").hide();
     $('#set_area_category').hide();
     $('#main_category_type').hide();
@@ -78,9 +77,9 @@ console.log(filename);
             $('#set_area_category').hide();
           });
 
-            $('.new_post').click(function() {
-              $('#set_area_category').hide();
-            });
+          $('.new_post').click(function() {
+            $('#set_area_category').hide();
+          });
         } else {
           $('#set_area_category').show();
           $('.new_post').click(function() {
@@ -131,7 +130,7 @@ console.log(filename);
           });
         }
         for (row in data) {
-          var newListItem = "<li><input type='checkbox' name='type' value='" + data[row].id + "'/>" + data[row].name + "</li>";
+          var newListItem = "<li><input id='new_post_" + data[row].id + "' type='checkbox' name='type' value='" + data[row].id + "'/><label for='new_post_" + data[row].id + "'>" + data[row].name + "</label></li>";
           $('.category_type').append(newListItem);
         }
       },
@@ -146,7 +145,79 @@ console.log(filename);
   $('#category_set').change(updateCategoryType);
 
 
-var filename = $('#image-upload').val();
+
+
+
+
+
+
+  $('#filter_category').change(function() {
+    $.ajax({
+      url: 'http://market48.toppy.in.th/api/category',
+      dataType: 'json',
+      type: 'GET',
+      // This is query string i.e. country_id=123
+      data: {
+        super_id: $('#filter_category').val()
+      },
+      success: function(data) {
+        if (data.length == 0) {
+          $("#filter_set_area_category").hide();
+        }else{
+          $("#filter_set_area_category").show();
+        }
+        $('#filter_set_area_category_input').empty(); // clear the current elements in select box
+        for (row in data) {
+          // console.log(row)
+          $('#filter_set_area_category_input').append($('<option></option>').attr('value', data[row].id).text(data[row].name));
+        }
+        updateFilterCategoryType();
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("w");
+      }
+    });
+  });
+
+
+  function updateFilterCategoryType() {
+    $.ajax({
+      url: 'http://market48.toppy.in.th/api/category',
+      dataType: 'json',
+      type: 'GET',
+      // This is query string i.e. country_id=123
+      data: {
+        super_id: $('#filter_set_area_category_input').val()
+      },
+      success: function(data) {
+        $('.filter_category_type').empty(); // clear the current elements in select box
+
+        if (data.length == 0) {
+          $("#filter_main_category_type").hide();
+        } else {
+          $("#filter_main_category_type").show();
+        }
+        for (row in data) {
+          var newListItem = "<li><input id='filter_" + data[row].id + "' type='checkbox' name='type' value='" + data[row].id + "'/><label for='filter_" + data[row].id + "'>" + data[row].name + "</label></li>";
+          $('.filter_category_type').append(newListItem);
+        }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert(errorThrown);
+      }
+    });
+  }
+
+
+  $('#filter_set_area_category_input').change(updateFilterCategoryType);
+  $("#filter_set_area_category").hide();
+
+
+
+
+
+
+  var filename = $('#image-upload').val();
   if (filename == "") {
     $("#image-preview").hide()
   }
@@ -166,6 +237,65 @@ var filename = $('#image-upload').val();
 
 
 
-  // Post
+  // Limit post
+  var minimized_elements = $('p.minimize');
+
+  minimized_elements.each(function() {
+    var t = $(this).text();
+    if (t.length < 200) return;
+
+    $(this).html(
+      t.slice(0, 200) + '<span> </span><br><a href="#" class="more">ดูเพิ่มเติม</a>' +
+      '<span style="display:none;">' + t.slice(100, t.length) + ' <br><a href="#" class="less">แสดงน้อยลง</a></span>'
+    );
+
+  });
+
+  $('a.more', minimized_elements).click(function(event) {
+    event.preventDefault();
+    $(this).hide().prev().hide();
+    $(this).next().show();
+  });
+
+  $('a.less', minimized_elements).click(function(event) {
+    event.preventDefault();
+    $(this).parent().hide().prev().show().prev().show();
+  });
+
+
+  //Scroll
+  // console.log($("div#post").length);
+  var filterDiv = $("#filter").offset().top;
+  $(window).scroll(function() {
+    //       // console.log($("div#post").length);
+    //       var n = 0;
+    //       if ($(window).scrollTop() > $("div#post").eq(n).offset().top){
+    //         console.log(n);
+    //       }else{
+    //         n++;
+    //       }
+    if ($(window).scrollTop() > filterDiv + 10) {
+      $('#filter').css({
+        'position': 'fixed',
+        'top': '10px',
+        'z-index': '1',
+        'width': '318px'
+      });
+    } else {
+      $('#filter').css({
+        'position': 'relative',
+        'top': 'auto'
+      });
+
+      console.log("1");
+    }
+  });
+
+  $(".contact_button").click(function(){
+    $(".contact_icon").fadeToggle();
+//     $(".contact_icon > ul > li").each(function(i) {
+//     $(this).delay(100 * i).fadeIn(500);
+// });
+  });
 
 });
